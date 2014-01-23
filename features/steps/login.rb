f98f30367d2e02db4c057be1c88fe779b9d6bd23
Alachheb <lachheb.mehrez@gmail.com>
2014-01-23 16:30:05 +0100

    Update login.rb

--- features/steps/login.rb
+++ features/steps/login.rb
@@ -1,15 +1,5 @@
 class Login < Spinach::FeatureSteps
 
-
-  # def Login.step(step, &block)
-  #   Spinach::FeatureSteps.step(step, &block)
-  # end
-  #define_singleton_method('Lorsque') { 'step' }
-
-  # def Lorsque(step, &block)
-  #   Spinach::FeatureSteps.step(step, &block)
-  # end
-
   Lorsque 'je me connecte en tant que admin' do
     visit '/'
     fill_in 'Username', :with => "user"
